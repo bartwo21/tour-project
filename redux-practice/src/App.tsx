8c3abd6b80ccdf14dece24173080d838faaefc6e
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-09-27 01:30:05 +0300

    fix: files

--- redux-practice/src/App.tsx
+++ redux-practice/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/home'
 
 function App() {
 
   return (
     <>
+    <nav>
+      
+    </nav>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </>
   )
---
+++ redux-practice/src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.scss'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div>home</div>
+  )
+}
+
+export default Home
