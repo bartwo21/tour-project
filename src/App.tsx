c61a77359a590b5783bcacd12136b5553ec74e1d
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-10-02 02:01:43 +0300

    feat: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/home'
+import Navbar from './constants/navbar/navbar'
 
 function App() {
 
   return (
-    <>
+    <div className='app'>
     <nav>
-      
+      <Navbar />
     </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
-    </>
+    </div>
   )
 }
 
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './home.scss'
+import Navbar from '../../constants/navbar/navbar'
+
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <div>home</div>
+    <>
+      <div>home</div>
+    </>
   )
 }
 
