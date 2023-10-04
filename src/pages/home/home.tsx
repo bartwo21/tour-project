bcc84ac62700152da5345f907bfe3616605f08a3
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-10-04 22:52:38 +0300

    feat: slider

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './home.scss';
 import img1 from './img/1.jpg';
-import img2 from './img/2.jpg';
 import img3 from './img/3.jpg';
 import img4 from './img/4.jpg';
 import img5 from './img/5.jpg';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 type Props = {};
 
@@ -16,15 +18,36 @@ type Card = {
   img: string;
 };
 
+type SliderCards = {
+  title: string;
+  description: string;
+  button: string; 
+}
+
 const Home: React.FC<Props> = ({}) => {
   const cards: Card[] = [
-    { size: 2, title: 'Card 1 Content', p: "Lorem ipsum dolor sit amet.", button: "Explore", img: img1 },
-    { size: 1, title: 'Card 2 Content', p: "Lorem ipsum dolor sit amet.", button: "Explore", img: "" },
-    { size: 1, title: 'Card 3 Content', p: "Lorem ipsum dolor sit amet.", button: "Explore", img: img3 },
-    { size: 1, title: 'Card 4 Content', p: "Lorem ipsum dolor sit amet.", button: "Explore", img: img4 },
-    { size: 1, title: 'Card 5 Content', p: "Lorem ipsum dolor sit amet.", button: "Explore", img: img5 },
+    { size: 2, title: 'Card 1 Content', p: "Lorem ipsum dolor sit amet.", button: "Browse by destination", img: img1 },
+    { size: 1, title: 'Card 2 Content', p: "Lorem ipsum dolor sit amet.", button: "Browse by destination", img: "" },
+    { size: 1, title: 'Card 3 Content', p: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.", button: "Browse by destination", img: img3 },
+    { size: 1, title: 'Card 4 Content', p: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.", button: "Browse by destination", img: img4 },
+    { size: 1, title: 'Card 5 Content', p: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.", button: "Browse by destination", img: img5 },
   ];
-  
+
+  const sliderCards: SliderCards[] = [
+    { title: "Experiential travel made easy", description: "You dream it. We’ll take care of every last detail.", button: "Find out how we do it" },
+    { title: "Pick your perfect trip", description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.", button: "" },
+    { title: "Put just $299 down", description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.", button: "" },
+    { title: "Prep your packing list", description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.", button: "" },
+    { title: "Say bon voyage!", description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.", button: "" },
+  ]
+  var settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3.5,
+    slidesToScroll: 2.5
+
+  };
   return (
     <div className="home">
       <h1 className="subtitle">Immersive guided tours, all around the globe</h1>
@@ -39,15 +62,27 @@ const Home: React.FC<Props> = ({}) => {
               className={`card card-${index + 1}`}
               style={{
                 flex: card.size,
-                backgroundImage: `url(${card.img})`, // Set background image
+                backgroundImage: `url(${card.img})`,
               }}
             >
-              <div className="background-image"></div>
               <p className="card-title">{card.title}</p>
               <p className="card-p">{card.p}</p>
               <span className="button">{card.button}</span>
             </div>
           ))}
+      </div>
+      <div className="slider-container">
+        <Slider {...settings}>
+          {sliderCards.map((card: SliderCards, index: number) => (
+            <div key={index} className={`card-s card-s-${index + 1}`}>
+              <div className="slider-card">
+                <p className="slider-card-title">{card.title}</p>
+                <p className="slider-card-p">{card.description}</p>
+                <span className="button">{card.button}</span>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
