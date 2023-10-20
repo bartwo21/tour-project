2199db92f1b6f42002ba1657bb388e6c484197e3
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-10-20 19:36:08 +0300

    feat: navbar routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,10 @@ function App() {
       <nav><Navbar /></nav>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/travel-deals' element={<h1>Travel Deals</h1>} />
+          <Route path='/destinations' element={<h1>Destinations</h1>} />
+          <Route path='/travelstyles' element={<h1>Travel Styles</h1>} />
+          <Route path='/aboutus' element={<h1>About Us</h1>} />
           <Route path="/sr" element={<SearchResultsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
--- src/pages/SearchResultsPage/SearchResultsPage.tsx
+++ src/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -3,21 +3,19 @@ import { useSearchParams } from "react-router-dom";
 import "./SearchResultsPage.scss";
 import SearchQueryPage from "../SearchQueryPage/SearchQueryPage";
 import searchs from "./Searchs";
+import NotFoundPage from "../notFound/NotFoundPage";
 
 const SearchResultsPage: React.FC = ({}) => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("q");
-  
+
   const isThereASearch = (query: string | null) => {
     const matchingSearch = searchs.find((search) => search.term === query);
     if (matchingSearch) {
       return <SearchQueryPage matchingSearch={matchingSearch} />;
     }
     return (
-      <p>
-        We couldn't find anything you were looking for with{" "}
-        <strong>{query || ""}</strong>
-      </p>
+      <NotFoundPage />
     );
   };
 
