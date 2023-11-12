e2a317d8ba8d7124231e0a44b0f3009d2aaa7817
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-11-12 21:54:20 +0300

    feat: reservation section added

--- src/pages/SearchQueryPage/SearchQueryPage.tsx
+++ src/pages/SearchQueryPage/SearchQueryPage.tsx
@@ -1,7 +1,9 @@
 import { AiOutlineHeart } from 'react-icons/ai';
-import React from 'react';
 import "./SearchQueryPage.scss";
-
+import { useReducer } from 'react';
+import { DateRangePicker } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+import reducer from "./reducer"
 type SearchQueryPageProps = {
   matchingSearch: MatchingSearch;
 }
@@ -19,7 +21,20 @@ type MatchingSearch = {
   button: string;
 };
 
+const initialState = {
+  date: null,
+  person: 0,
+  nameSurname: '',
+  email: '',
+  ticket: '',
+}
+
 const SearchQueryPage = ({ matchingSearch }: SearchQueryPageProps) => {
+  const [state , dispatch] = useReducer(reducer, initialState);
+  const handleSubmit = () => {
+    console.log(state);
+  }
+
   return (
     <div className='container'>
       <div className="content">
@@ -39,9 +54,6 @@ const SearchQueryPage = ({ matchingSearch }: SearchQueryPageProps) => {
                 <p className='review-title'>Reviews</p>
                 <div className="stars">{matchingSearch.stars} stars</div>
               </div>
-              <div className="button">
-                <button>Reservation</button>
-              </div>
               <span className="heart-button"><AiOutlineHeart size={40} className="heart"/></span>
             </div>
           </div>
@@ -58,6 +70,68 @@ const SearchQueryPage = ({ matchingSearch }: SearchQueryPageProps) => {
           </div>
         </div>
       <p className='desc'>{matchingSearch.description}</p>
+      </div>
+      <div className="reservation">
+        <div className="inputs">
+          <div className="row">
+            <div className="box">
+              <h3>Location</h3>
+              <input type="text" disabled className="location" value={matchingSearch.title} />
+            </div>
+            <div className="box">
+              <h3>Date</h3>
+              <DateRangePicker format="dd-MM-yyyy" placeholder="Select Date Range" name='date' onChange={
+                  (e) => dispatch({ type: 'SET_DATE', payload: e })  
+              } />
+            </div>
+            <div className="box">
+              <h3>Person</h3>
+              <input
+                type="number"
+                className="person"
+                value={state.person}
+                onChange={(e) => {
+                  const inputValue = parseInt(e.target.value, 10);
+                  if (!isNaN(inputValue) && inputValue >= 0) {
+                    dispatch({ type: 'SET_PERSON', payload: inputValue });
+                  }
+                }}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="box">
+              <h3>Name & Surname</h3>
+              <input type="text" className="name-surname" onChange={
+                  (e) => dispatch({ type: 'SET_NAMESURNAME', payload: e.target.value }) 
+              }/>
+            </div>
+            <div className="box">
+              <h3>Email</h3>
+              <input type="text" className="email" onChange={
+                  (e) => dispatch({ type: 'SET_EMAIL', payload: e.target.value }) 
+              }/>
+            </div>
+            <div className="box">
+              <h3>Ticket</h3>
+              <div className="button-box">
+                <input type="radio" value="our" name="flight" className='radio-button' onChange={
+                  (e) => dispatch({ type: 'SET_TICKET', payload: e.target.value })
+                }/>
+                <p>Book our airfare package from</p>
+              </div>
+              <div className="button-box">
+                <input type="radio" value="self" name="flight" className='radio-button' onChange={
+                  (e) => dispatch({ type: 'SET_TICKET', payload: e.target.value })
+                }/>
+                <p>I will book my own flights</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="button">
+          <button onClick={handleSubmit}>Reservation</button>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/SearchQueryPage/reducer.tsx
@@ -0,0 +1,17 @@
+const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'SET_DATE':
+            return {...state , date: action.payload}
+        case 'SET_PERSON':
+            return {...state , person: action.payload}
+        case 'SET_NAMESURNAME':
+            return {...state , nameSurname: action.payload}
+        case 'SET_EMAIL':
+            return {...state , email: action.payload}
+        case 'SET_TICKET':
+            return {...state , ticket: action.payload}
+        default: return state;
+    }
+}
+
+export default reducer;
