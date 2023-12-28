b33381cf192b2bfc8d38f6712044b9d7548db61a
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-12-28 21:34:29 +0300

    fix: img loadings

--- components/TravelCard/TravelCard.tsx
+++ components/TravelCard/TravelCard.tsx
@@ -1,10 +1,14 @@
-import { AiOutlineHeart } from 'react-icons/ai';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser, toggleFavoriteCard, selectFavoriteCards } from '../../src/store/features/authSlice/authSlice';
-import { useNavigate } from 'react-router-dom';
-import { motion, useInView, useAnimation } from 'framer-motion';
-import { useEffect, useRef } from 'react';
-import './TravelCard.scss';
+import { AiOutlineHeart } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectUser,
+  toggleFavoriteCard,
+  selectFavoriteCards,
+} from "../../src/store/features/authSlice/authSlice";
+import { useNavigate } from "react-router-dom";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
+import "./TravelCard.scss";
 
 type TravelCards = {
   id: number;
@@ -20,14 +24,14 @@ type TravelCards = {
   favorite: boolean;
 };
 
-const TravelCard = ({ card, index }: { card: TravelCards, index: number}) => {
+const TravelCard = ({ card, index }: { card: TravelCards; index: number }) => {
   const user = useSelector(selectUser);
-  const favoriteCards  = useSelector(selectFavoriteCards);
+  const favoriteCards = useSelector(selectFavoriteCards);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const ref = useRef(null);
-  const isInView = useInView(ref, {once: true});
+  const isInView = useInView(ref, { once: true });
 
   const mainControls = useAnimation();
 
@@ -38,46 +42,71 @@ const TravelCard = ({ card, index }: { card: TravelCards, index: number}) => {
   }, [isInView]);
 
   return (
-          <motion.div
-          ref={ref}
-          variants={{
-            hidden: { opacity: 0, y: 25 },
-            visible: { opacity: 1, y: 0 },
-          }}
-          initial="hidden"
-          animate={mainControls}
-          transition={{ duration: 0.4 }}
-          key={index} className={`card-t card-t-${index + 1}`}>
-            <div className="travel-card">
-              <div className="image-container">
-                {user && <span onClick={
-                  () => {
-                    dispatch(toggleFavoriteCard(card.id))
-                  }
-                } className={
-                  favoriteCards.includes(card.id) ? "heart-button heart-button-active" : "heart-button"
-                }><AiOutlineHeart className="heart" /></span>}
-                <div className="travel-card-img">
-                  <img src={card.img} alt="" />
-                </div>
-                <div className="travel-card-map">
-                  <img src={card.map} alt="" />
-                </div>
-              </div>
-              <div className="text-container">
-                <p className="title">{card.title}</p>
-                <p className="description">{card.description}</p>
-                <p className="description2">Fuga hic quas veritatis blanditiis.</p>
-                <div className="bottom-row">
-                  <div className="prices">
-                    <span className="old-price">From <span>${card.oldPrice}</span></span>
-                    <span className="new-price">${card.price}</span>
-                  </div>
-                  <span className="button" onClick={() => {navigate(`/${card.url}`)}}>{card.button}</span>
-                </div>
-              </div>
+    <motion.div
+      ref={ref}
+      variants={{
+        hidden: { opacity: 0, y: 25 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      initial="hidden"
+      animate={mainControls}
+      transition={{ duration: 0.4 }}
+      key={index}
+      className={`card-t card-t-${index + 1}`}
+    >
+      <div className="travel-card">
+        <div className="image-container">
+          {user && (
+            <span
+              onClick={() => {
+                dispatch(toggleFavoriteCard(card.id));
+              }}
+              className={
+                favoriteCards.includes(card.id)
+                  ? "heart-button heart-button-active"
+                  : "heart-button"
+              }
+            >
+              <AiOutlineHeart className="heart" />
+            </span>
+          )}
+          <div className="travel-card-img">
+            <img src={card.img} alt="" />
+          </div>
+          <div className="travel-card-map">
+            <img
+              src={card.map}
+              alt=""
+              onLoad={() => {
+                mainControls.start("visible");
+                console.log("loaded");
+              }}
+            />
+          </div>
+        </div>
+        <div className="text-container">
+          <p className="title">{card.title}</p>
+          <p className="description">{card.description}</p>
+          <p className="description2">Fuga hic quas veritatis blanditiis.</p>
+          <div className="bottom-row">
+            <div className="prices">
+              <span className="old-price">
+                From <span>${card.oldPrice}</span>
+              </span>
+              <span className="new-price">${card.price}</span>
             </div>
-          </motion.div>
+            <span
+              className="button"
+              onClick={() => {
+                navigate(`/${card.url}`);
+              }}
+            >
+              {card.button}
+            </span>
+          </div>
+        </div>
+      </div>
+    </motion.div>
   );
 };
 
