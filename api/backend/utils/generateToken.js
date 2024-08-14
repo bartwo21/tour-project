f44abc043a66d7fcef889f2c727dc2de7dc3a274
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-08-14 20:28:32 +0300

    fix

--- api/backend/utils/generateToken.js
+++ api/backend/utils/generateToken.js
@@ -8,7 +8,6 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
