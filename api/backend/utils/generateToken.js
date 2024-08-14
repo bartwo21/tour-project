95661a4f91f0d7d5af270bfa79e555deb8e91f72
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-08-14 20:45:36 +0300

    fix

--- api/backend/utils/generateToken.js
+++ api/backend/utils/generateToken.js
@@ -4,12 +4,11 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-  console.log(token);
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV !== "development",
-    secure: false,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
