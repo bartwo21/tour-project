0a43448b22f4719ba00656cd65357d26c5ed8102
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-08-14 20:42:25 +0300

    fix

--- api/backend/utils/generateToken.js
+++ api/backend/utils/generateToken.js
@@ -4,6 +4,7 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
+  console.log(token);
 
   res.cookie("jwt", token, {
     httpOnly: true,
@@ -11,7 +12,6 @@ const generateToken = (res, userId) => {
     secure: false,
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
-  console.log(token);
 };
 
 module.exports = generateToken;
